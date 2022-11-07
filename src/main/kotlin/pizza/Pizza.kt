ca0b03401b327957be606d2fa743ec4c9a05137f
Evgeny <mousedead21@gmail.com>
2022-11-07 14:32:21 +0300

    A bit more sense to pizza classes

--- src/main/kotlin/pizza/Pizza.kt
+++ src/main/kotlin/pizza/Pizza.kt
@@ -1,29 +1,30 @@
 package pizza
 
 abstract class Pizza(
-    val isOpen: Boolean,
-    val isSpicy: Boolean,
-    val ingredients: Ingredients
+    val isOpen: Boolean, val isSpicy: Boolean, val ingredients: Ingredients
 )
 
 data class Ingredients(
-    val cheese: Boolean = true,
-    val meat: Boolean = true,
-    val mushrooms: Boolean = true
+    val cheese: Boolean = true, val meat: Boolean = true, val mushrooms: Boolean = true
 )
 
-object Margarita :
-    Pizza(isOpen = true, isSpicy = false, ingredients = Ingredients(mushrooms = false, meat = false))
+object Margarita : Pizza(isOpen = true, isSpicy = false, ingredients = Ingredients(mushrooms = false, meat = false)) {
+    override fun toString(): String = "Margarita!"
+}
 
-object Romana :
-    Pizza(isOpen = true, isSpicy = false, ingredients = Ingredients())
+object Romana : Pizza(isOpen = true, isSpicy = false, ingredients = Ingredients()) {
+    override fun toString(): String = "Romana!"
+}
 
-object Calzone :
-    Pizza(isOpen = false, isSpicy = true, ingredients = Ingredients(mushrooms = false))
+object Calzone : Pizza(isOpen = false, isSpicy = true, ingredients = Ingredients(mushrooms = false)) {
+    override fun toString(): String = "Calzone!"
+}
 
 class MyCustomPizza(isOpen: Boolean, isSpicy: Boolean, ingredients: Ingredients) : Pizza(
     isOpen, isSpicy, ingredients
-)
+) {
+    override fun toString(): String = "We don't know such pizza, but we will make it!"
+}
 
 object Pizzeria {
     fun createPizza(
@@ -31,9 +32,7 @@ object Pizzeria {
         isSpicy: Boolean = false,
         ingredients: Ingredients = Ingredients(mushrooms = false, meat = false)
     ): Pizza {
-        if (isOpen && !isSpicy && ingredients ==
-            Ingredients(mushrooms = false, meat = false)
-        ) return Margarita
+        if (isOpen && !isSpicy && ingredients == Ingredients(mushrooms = false, meat = false)) return Margarita
         if (!isOpen && isSpicy && ingredients == Ingredients(mushrooms = false)) return Calzone
         if (isOpen && !isSpicy && ingredients == Ingredients()) return Romana
         return MyCustomPizza(isOpen, isSpicy, ingredients)
